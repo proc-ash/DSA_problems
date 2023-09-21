6d3f223a9e3ebc7e56f56a491021b858c3d2d371
Prakash Tripathi <prakash.nopany@gmail.com>
2023-09-21 21:20:21 +0530

    completed all easy problems

--- striver/strings/Easy/single_rotation.py
+++ striver/strings/Easy/single_rotation.py
@@ -1,8 +1,12 @@
 def isRotated(p,q):
     if len(p) != len(q):
-        return False
-    
-    pass
+        return 0
+    for i in range(len(p)):
+        if p == q:
+            return 1
+        p = p[1:] + p[0]
+    return 0
+
 if __name__ =='__main__':
     p = 'abac'
     q = 'baca'
